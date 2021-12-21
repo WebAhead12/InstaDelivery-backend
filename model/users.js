0713139bdf0cca6b48af1dbd55ca4af4db77064e
Mohammad Faur <mohammad.fa@live.com>
2021-12-22 01:06:16 +0200

    fixes in register/login functionality

--- model/users.js
+++ model/users.js
@@ -1,41 +1,46 @@
-const db = require(".../database/connection");
+const db = require("../database/connection");
 
-const getUser = (user) => {
+//get user row frm database
+const getUser = (email) => {
   return db.query(`SELECT * FROM users`).then((result) => {
-    const users = result.rows;
+    const users = result.rows; //[{account1},{account2}...]
 
     if (!users.length)
-      throw new Error(`Something went wrong, cannot get data from database`);
-    return users.filter((account) => account.email == user);
+      throw new Error(`Something wrong, could not get data from the database`);
+    return users.filter((account) => account.email == email); //array with one account if exists
   });
 };
 
+//get User ID
 const getUserID = (user) => {
   return getUser(user).then((accArray) => {
+    //account array of 1 item
     const [account] = accArray;
     if (!accArray.length) {
-      throw new Error(`This ${user} does not exist`);
+      throw new Error(`This ${user} doesn't exist`);
     } else {
       return account.id;
     }
   });
 };
 
-const setUser = (account) => {
+//insert registered user to database
+const createUser = (account) => {
   const values = [account.email, account.name, account.password];
   return db.query(
-    `INSERT INTO users (email, name, password) VALUES ($1, $2, $3)`,
+    `INSERT INTO users(email, name, password) VALUES($1, $2, $3)`,
     values
   );
 };
 
+//get user info by id
 const getUserInfoByID = (id) => {
   return db
-    .query(`SELECT id, username, name FROM users WHERE id = ${id})`)
+    .query(`SELECT id, email, name FROM users WHERE id = ${id}`)
     .then((result) => {
       const userInfo = result.rows;
       return userInfo;
     });
 };
 
-module.exports = { getUser, setUser, getUserInfoByID, getUserID };
+module.exports = { getUser, createUser, getUserInfoByID, getUserID };
